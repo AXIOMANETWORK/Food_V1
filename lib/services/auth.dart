56baa0640f07b53c9cf3d77ed5d7502746aff8f3
JACisneros94 <jacisnerosp.94@gmail.com>
2021-07-26 22:59:22 -0500

    Cambios 24/07/21 v3 para entrega 02/08/21 - Login por Google

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+//import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth {
@@ -24,7 +24,6 @@ class Auth {
   iniciarSesion(email, password) async {
     String error = '';
     try {
-      //auth.signInWithEmailAndPassword(email: email, password: password).then((UserCredential user) => );
       await auth.signInWithEmailAndPassword(email: email, password: password);
     } on FirebaseException catch (e) {
       if (e.code == 'user-not-found') {
@@ -34,19 +33,6 @@ class Auth {
       }
     }
     return error;
-  }
-
-  Future<UserCredential> signInWithFacebook() async {
-    // Trigger the sign-in flow
-    final AccessToken result = await FacebookAuth.instance.login();
-
-    // Create a credential from the access token
-    final facebookAuthCredential =
-        FacebookAuthProvider.credential(result.token);
-
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance
-        .signInWithCredential(facebookAuthCredential);
   }
 
   Future<UserCredential> signInWithGoogle() async {
